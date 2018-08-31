9181f17faebc0907da416d828817854e303d1a26
davidlundholm <pakanatahu@outlook.com>
2018-09-01 01:56:46 +0200

    sqlalchemy

--- app_config.py
+++ app_config.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 # Returns app config.
 def get_app_config():
 
     # App start.
     app = Flask(__name__)
-
-    # Config MySQL
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'password'
-    app.config['MYSQL_DB'] = 'myflaskapp'
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    app.config['MYSQL_PORT'] = 3300
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/myflaskapp'
 
     return app
