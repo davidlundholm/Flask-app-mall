07591c7fa71aea8c8d81686754154a21d5ee2335
davidlundholm <pakanatahu@outlook.com>
2018-09-01 21:15:55 +0200

    asda

--- models.py
+++ models.py
@@ -1,10 +1,3 @@
 
 
-class Example(db.Model):
-    __tablename__ = 'example'
-    id = db.Column('id', db.Integer, primary_key=True)
-    data = db.Column('data', db.Unicode)
 
-    def __init__(self, id, data):
-        self.id = id
-        self.data = data
